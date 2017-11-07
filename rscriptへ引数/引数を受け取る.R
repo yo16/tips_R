12d3abcfa1a04eb34cccdb7d9e01a83cf0cee35c
yo16 <xxyo1xx@gmail.com>
2017-11-07 12:59:47 +0900

    11/7

---
+++ rscriptへ引数/引数を受け取る.R
@@ -0,0 +1,13 @@
+
+# 引数を受け取る
+# 2017/11/7 y.ikeda
+
+args1 = commandArgs(trailingOnly=TRUE)[1]
+args2 = commandArgs(trailingOnly=TRUE)[2]
+args3 = commandArgs(trailingOnly=TRUE)[3]
+
+paramArray = c(args1,args2,args3)
+
+table(paramArray)
+
+
---
+++ rscriptへ引数/引数を受け取る_実行.bat
@@ -0,0 +1,8 @@
+@echo off
+set path=%PATH%;C:\Program Files\R\R-3.4.2\bin\x64
+@echo on
+
+rscript ˆø”‚ğó‚¯æ‚é.r 10 20 30
+
+pause
+
