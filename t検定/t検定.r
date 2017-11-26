f7a223a16c94b3b675a6dea84208e805c917f357
yoichiro <xxikedaxx@yahoo.co.jp>
2017-11-26 22:15:21 +0900

    11/26

--- t検定/t検定.r
+++ t検定/t検定.r
@@ -3,7 +3,7 @@
 
 # 下記が、平均170の母集団から抽出した無作為標本と考えてよいか？
 # 有意水準5%でt検定
-meanOfPopulation <- 170
+meanOfPopulation = 170
 # 有意水準(両側)
 sigLevel = 0.05
 # データ
@@ -11,23 +11,31 @@ data <- c(165,150,170,168,159,170,167,178,155,159,161,162,166,171,155,160,168,17
 
 # ------------------------
 
+# 標本平均
+meanOfSample = mean(data)
+print(paste("mean of sample:", meanOfSample, sep=""))
+
 # 検定統計量
 t = (mean(data) - meanOfPopulation) / (sqrt(var(data))/sqrt(length(data)))
-print(paste("t:",t))
+print(paste("t:",t, sep=""))
 # [1] "t: -3.8502858588966"
 
 # 自由度
 df = length(data)-1
-# 棄却域
+print(paste("df:",df, sep=""))
+
+# 棄却域(critical region)
 rangeMin <- qt(sigLevel/2, df)
 #rangeMax <- qt(1-sigLevel/2, df)
 rangeMax <- qt(sigLevel/2, df, lower.tail=FALSE)	# 上側確率0.025となるtの値を求める
-print(paste("棄却域:", rangeMin, "以下 または ", rangeMin, "以上")){
+print(paste("critical region: t<", rangeMin, " or ", rangeMin, "<t", sep=""))
 if ( (rangeMin<t) && (t<rangeMax) ) {
-	print("t検定の結果:採択域")
+	print("t-test result: SAITAKU")
 }else{
-	print("t検定の結果:棄却域")
+	print("t-test result: REJECT!")
 }
 # -----------------------------------
 print("----------------")
 t.test( data, mu=170 )
+
+
--- t検定/t検定_実行.bat
+++ t検定/t検定_実行.bat
@@ -1,5 +1,4 @@
 @echo off
-chcp 65001
 
 set RSC="C:\Program Files\R\R-3.4.2\bin\x64\Rscript.exe"
 
